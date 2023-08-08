f8f1e9cebf043ba12e004bde35159ac07edf9361
FractalTeamBot <hello@fractal.software>
2023-08-08 17:15:03 -0600

    Corregida la consulta y retorno

--- server/consulta.php
+++ server/consulta.php
@@ -1,18 +1,26 @@
 <?php
-function ejecutarConsulta() {
-    global $conn;
-
-    $sql = "SELECT t1.campo1, t1.campo2, t2.campo1, t2.campo2, t3.campo1, t3.campo2
-            FROM tabla1 t1
-            JOIN tabla2 t2 ON t1.id = t2.id
-            JOIN tabla3 t3 ON t1.id = t3.id";
+    require_once("./db.php");
+    
+    $sql = "SELECT t1.campo1 AS t1_campo1, t1.campo2 AS t1_campo2, 
+            t2.campo2 AS t2_campo2, t2.campo3 AS t2_campo3, 
+            t3.campo3 AS t3_campo3, t3.campo4 AS t3_campo4
+            FROM tabla1 AS t1
+            JOIN tabla2 AS t2 ON t1.id = t2.id
+            JOIN tabla3 AS t3 ON t1.id = t3.id";
 
     $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        return $result->fetch_all(MYSQLI_ASSOC);
+    if ($result) {
+        while ($row = $result->fetch_assoc()) {
+            $data[] = array(
+                "tabla1" => array("campo1" => $row['t1_campo1'], "campo2" => $row['t1_campo2']),
+                "tabla2" => array("campo1" => $row['t2_campo2'], "campo2" => $row['t2_campo3']),
+                "tabla3" => array("campo1" => $row['t3_campo3'], "campo2" => $row['t3_campo4'])
+            );
+        }
     } else {
-        return [];
+        echo "Error en la consulta: " . $conn->error;
     }
-}
+    
+    $conn->close();
+    echo json_encode($data);
 ?>
