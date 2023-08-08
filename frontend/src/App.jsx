963d020455f04666ff1332902022fb9bff1d194d
FractalTeamBot <hello@fractal.software>
2023-08-08 17:46:22 -0600

    Agregada la tabla para mostrar resultado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,6 +28,43 @@ function App() {
             Consulta
           </button>
           <hr />
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th colSpan={2} className="border">
+                  Tabla 1
+                </th>
+                <th colSpan={2} className="border">
+                  Tabla 2
+                </th>
+                <th colSpan={2} className="border">
+                  Tabla 3
+                </th>
+              </tr>
+              <tr>
+                <th className="border">Campo 1</th>
+                <th className="border">Campo 2</th>
+                <th className="border">Campo 2</th>
+                <th className="border">Campo 3</th>
+                <th className="border">Campo 3</th>
+                <th className="border">Campo 4</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((row, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{row.tabla1.campo1}</td>
+                    <td>{row.tabla1.campo2}</td>
+                    <td>{row.tabla2.campo1}</td>
+                    <td>{row.tabla2.campo2}</td>
+                    <td>{row.tabla3.campo1}</td>
+                    <td>{row.tabla3.campo2}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
